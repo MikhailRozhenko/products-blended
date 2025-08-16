855794c184e926f82da1519e6dfcdc2bda863cb0
Eduard <nikinna86@gmail.com>
2025-08-16 12:13:47 +0300

    init products

--- src/js/handlers.js
+++ src/js/handlers.js
@@ -1,7 +1,14 @@
 import iziToast from 'izitoast';
-import { fetchCategories } from './products-api';
-import { renderCategories } from './render-function';
-import { activeFirstBtn } from './helpers';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import { fetchCategories, fetchProducts } from './products-api';
+import { renderCategories, renderProducts } from './render-function';
+import { activeFirstBtn, iziToastErrorMessage } from './helpers';
+import { PER_PAGE } from './constants';
+import { refs } from './refs';
+
+let currentPage = 1;
+let totalProducts = 0;
 
 export async function getCategories() {
   try {
@@ -12,4 +19,33 @@ export async function getCategories() {
     console.log(error);
     iziToast.error({ message: 'Try again later!' });
   }
+}
+
+export async function getAllProducts() {
+  try {
+    const data = await fetchProducts(currentPage);
+    totalProducts = data.total;
+    console.log(data.total);
+    renderProducts(data.products);
+
+    if (totalProducts > PER_PAGE) {
+      refs.loadMoreBtn.classList.remove('is-hidden');
+      refs.loadMoreBtn.addEventListener('click', loadMoreProducts);
+    } else {
+      refs.loadMoreBtn.classList.add('is-hidden');
+      refs.loadMoreBtn.removeEventListener('click', loadMoreProducts);
+    }
+    totalProducts -= 12;
+  } catch (error) {
+    console.log(error);
+    iziToastErrorMessage(error);
+  }
+}
+
+export async function loadMoreProducts() {
+  currentPage += 1;
+
+  // if (pageState.categoryName === 'All' || pageState.categoryName === '') {
+  //   fetchProducts();
+  // }
 }
--- src/js/render-function.js
+++ src/js/render-function.js
@@ -11,3 +11,26 @@ export function renderCategories(data) {
     .join('');
   refs.categoriesList.innerHTML = markup;
 }
+
+export function renderProducts(products) {
+  const markup = products
+    .map(
+      ({
+        id,
+        thumbnail,
+        title,
+        brand,
+        category,
+        price,
+        description,
+      }) => `<li class="products__item" data-id="${id}">
+    <img class="products__image" src="${thumbnail}" alt="${description}"/>
+    <p class="products__title">${title}</p>
+    <p class="products__brand"><span class="products__brand--bold">Brand: ${brand}</span></p>
+    <p class="products__category">Category: ${category}</p>
+    <p class="products__price">Price: ${price}$</p>
+ </li>`
+    )
+    .join('');
+  refs.productList.insertAdjacentHTML('beforeend', markup);
+}
